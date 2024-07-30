80114fd6401089ee760e9cf859d618dcffc8d4ee
Alexandre Ballardin <aleballa08@gmail.com>
2024-07-30 19:30:35 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let formulario = document.querySelector(".formulario")
+function aparecerform() {
+    formulario.style.left = "35%"
+
+
+}
+
+function desaparecerform() {
+    formulario.style.left = "-50%"
+
+}
+
